e99ec3dfff3fa07f179546506a9494c64956e1dc
martinkro <martinkro@aliyun.com>
2016-08-04 20:25:19 +0800

    Button

---
+++ Assets/Water2D/Code/FileLog.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public sealed class FileLog {
+
+	private static volatile FileLog _instance;
+	private string _logPath = Application.persistentDataPath + "/tdebug.log";
+	private static object _lock = new object();
+	public static FileLog Instance{
+		get{
+			if (_instance == null){
+				lock(_lock){
+					if (_instance == null){
+						_instance = new FileLog();
+					}
+				}
+			
+			}
+			return _instance;
+		}
+	}
+
+	private FileLog(){
+		if (File.Exists (_logPath)) {
+			File.Delete(_logPath);
+		}
+	}
+
+	public void Log(string format, params object[] args){
+		DateTime dt = DateTime.Now;
+		string time = dt.ToString ("yyyy/MM/dd HH:mm:ss") + " " + dt.Millisecond.ToString ();
+		string message = string.Format (format, args);
+		message = "[" + time + "]" + message;
+		FileStream file = new FileStream (_logPath, FileMode.Append);
+		if (file != null) {
+			StreamWriter writer = new StreamWriter(file);
+			writer.WriteLine (message);
+			writer.Close ();
+			file.Close ();
+		}
+	}
+}
--- Assets/Water2D/Code/WaterTester.cs
+++ Assets/Water2D/Code/WaterTester.cs
@@ -7,6 +7,11 @@ public class WaterTester : MonoBehaviour {
 	public Water2D water;
 	public float force;
 	public int size = 0;
+
+	private static uint screenshot1Count = 0;
+	private static uint screenshot1Count1 = 0;
+	private static uint screenshot1Count2 = 0;
+	private static uint screenshot1Count3 = 0;
 	
 	//public float waterHeightChanger = 100;
 	
@@ -39,5 +44,91 @@ public class WaterTester : MonoBehaviour {
 		}
 	//water.SetHeight(waterHeightChanger);
 	
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (Screen.width/2f - 200f, 0f, 200, 100), "Capture1")) {
+			Debug.Log ("Button capture1 with Application");
+			Debug.Log ("Path:" + Application.persistentDataPath);
+			Debug.Log ("Path:" + Application.dataPath);
+			FileLog.Instance.Log ("Path:" + Application.persistentDataPath);
+			CaptureScreenshot1();
+
+		}
+
+		if (GUILayout.Button ("Capture 1", GUILayout.Width(300), GUILayout.Height (100))) {
+			string ImageName = "screenshot1-" + screenshot1Count1 + ".png";
+			screenshot1Count1++;
+
+			CaptureByUnity(ImageName);
+		}
+
+		if (GUILayout.Button ("Capture 2", GUILayout.Width(300), GUILayout.Height (100))) {
+			string ImageName = "screenshot2-" + screenshot1Count2 + ".png";
+			screenshot1Count2++;
+			string ImagePath = Application.persistentDataPath + "/" + ImageName;
+			Debug.Log ("Path:" + Application.dataPath);
+			StartCoroutine(CaptureByRect(new Rect(0,0,Screen.width, Screen.height),ImagePath));
+			
+		}
+
+		if (GUILayout.Button ("Capture 3", GUILayout.Width(300), GUILayout.Height (100))) {
+			Debug.Log ("Path:" + Application.dataPath);
+				//string ImageName = "screenshot3-" + screenshot1Count3 + ".png";
+				//screenshot1Count3++;
+				//string ImagePath = Application.persistentDataPath + "/" + ImageName;
+				
+			//Transform CameraTrans;
+			//CameraTrans.camera.enabled = true;
+			//	StartCoroutine(CaptureByCamera(new Rect(0,0,Screen.width, Screen.height),ImagePath));
+		}
+	}
+
+	static void CaptureByUnity(string imageName){
+		Application.CaptureScreenshot (imageName, 0);
+	}
+
+	IEnumerator CaptureByRect(Rect rect, string imageName){
+		yield return new WaitForEndOfFrame ();
+
+		Texture2D texture = new Texture2D ((int)rect.width, (int)rect.height, TextureFormat.RGB24, false);
+		texture.ReadPixels (rect, 0, 0);
+		texture.Apply ();
+
+		byte[] bytes = texture.EncodeToPNG ();
+
+		System.IO.File.WriteAllBytes (imageName, bytes);
+	}
+
+	IEnumerator CaptureByCamera(Camera camera, Rect rect, string imageName){
+		yield return new WaitForEndOfFrame ();
+
+		RenderTexture texture = new RenderTexture ((int)rect.width, (int)rect.height, 0);
+
+		camera.targetTexture = texture;
+
+		camera.Render ();
+		RenderTexture.active = texture;
+
+		Texture2D texture2d = new Texture2D ((int)rect.width, (int)rect.height, TextureFormat.RGB24, false);
+		texture2d.ReadPixels (rect, 0, 0);
+		texture2d.Apply ();
+
+		camera.targetTexture = null;
+		RenderTexture.active = null;
+		GameObject.Destroy (texture);
+		
+		byte[] bytes = texture2d.EncodeToPNG ();
+		
+		System.IO.File.WriteAllBytes (imageName, bytes);
+
+	}
+
+	static void CaptureScreenshot1(){
+		string ImageName = "screenshot-" + screenshot1Count + ".png";
+		// string ImagePath = Application.persistentDataPath + "/" + ImageName;
+		screenshot1Count++;
+		Application.CaptureScreenshot(ImageName);
 	}
 }
